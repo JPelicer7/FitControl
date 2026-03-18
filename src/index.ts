28032c79af909b0c777aa4f55d6efe98ffc8cfc2
João Pelicer <jpelicer12@yahoo.com>
2026-03-18 09:49:03 -0300

    feat: integrate Fastify with Swagger and Zod for API management

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,70 @@
-console.log("Hello")
+import "dotenv/config";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+// Import the framework and instantiate it
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+// Declare a route
+app.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    querystring: z.object({
+      name: z.string().min(4),
+    }),
+    response: {
+      200: z.string(),
+    },
+  },
+  handler: (req, res) => {
+    res.send(req.query.name);
+  },
+});
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "FitControl",
+      description: "API para Gerenciamento de Academia",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "LocalHost",
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+// Run the server!
+try {
+  await app.listen({ port: Number(process.env.PORT || 8080) });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
